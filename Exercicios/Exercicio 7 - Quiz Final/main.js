9c3bda71554c7a0368bfb1677fcfd2fb601569e4
Gustavo-Teotonio <gustavohidroar07@gmail.com>
2024-06-15 23:14:57 -0300

    Tarefa final junto da alterações no html

--- Exercicios/Exercicio 7 - Quiz Final/main.js
+++ Exercicios/Exercicio 7 - Quiz Final/main.js
@@ -13,18 +13,19 @@ let valorBackgroundColor = ""
 let valorZindex = ""
 let valorLeft = ""
 let atravessou = 0
+let parteRasa = 0
 
 valorTopInput.addEventListener("keyup", (e)=>{
     valorTop = valorTopInput.value.toLowerCase().trim()
     let srcPedra = pedra.children[0].outerHTML.substring(5, 28)
     
     if(srcPedra.includes("pedra")){
-        if(valorTop >= 280 && valorTop <= 300){
+        if(valorTop >= 180 && valorTop <= 200){
             princesa.style.top = `${valorTop}px`
             setTimeout(() => {
                 window.alert("Cuidado, obstáculo a frente! Use o background-color para aumentar a sua aurea, quebrando o obstaculo!")
             }, 1000)
-        }else if(valorTop > 300){
+        }else if(valorTop > 200){
             window.alert("Valor muito alto")
         }else if(valorTop < 0){
             window.alert("Valor muito baixo")
@@ -33,9 +34,9 @@ valorTopInput.addEventListener("keyup", (e)=>{
         }
         mudaPedra()
     }else{
-        if(valorTop > 780){
+        if(valorTop > 660){
             window.alert("Valor muito alto")
-        }else if (valorTop >= 740 && valorTop < 780){
+        }else if (valorTop >= 630 && valorTop < 660){
             princesa.style.top = `${valorTop}px`
             setTimeout(() => {
                 window.alert("Você conseguiu chegar na parte mais rasa do rio!")
@@ -46,15 +47,16 @@ valorTopInput.addEventListener("keyup", (e)=>{
     }
 
     if(atravessou == 1){
-        window.alert("entrou")
-        if((valorTop >= 0 && valorTop <= 200) && (valorTop >= 460 && valorLeft <= 500)){
+        if(valorTop >= 0 && valorTop <= 150){
             persistencia()
             princesa.style.top = "18px"
             princesa.style.left = "490px"
+            princesa.style.borderRadius = ''
+            princesa.style.background = ''
             setTimeout(() => {
                 modal.showModal()
                 bntVoltar.addEventListener("click", ()=>{
-                    window.location.href = "../../Mapas/MapaExplorar/mapaExplorar.html"
+                    window.location.href = "../../Mapas/MapaHistoria/mapaHistoria.html"
                 })
             }, 1500)
         }
@@ -62,13 +64,12 @@ valorTopInput.addEventListener("keyup", (e)=>{
 })
 
 function mudaPedra(){
-    if(valorBackgroundColor == "blue" && (valorTop >= 280 && valorTop <= 300)){
+    if(valorBackgroundColor == "purple" && (valorTop >= 180 && valorTop <= 200)){
         const pedras = document.querySelectorAll(".pedra")
 
         pedras.forEach(pedra =>{
             console.log(pedra.innerHTML)
             pedra.setAttribute("src", "../../assets/Pedra Quebrada.png")
-            textoPedras.textContent = "Após quebrar as pedras, você deve passar por cima delas. E quando você estiver na parte mais rasa do rio, você deve atravessar ele"
         })
         setTimeout(() => {
             window.alert("Ufa, agora que as pedras estão quebradas, você pode descer com a princesa!")
@@ -80,10 +81,18 @@ function mudaPedra(){
 
 valorBackgroundColorInput.addEventListener("keyup", ()=>{
     valorBackgroundColor = valorBackgroundColorInput.value.toLowerCase().trim()
-    if(valorBackgroundColor == "blue"){
-        princesa.style.backgroundColor = valorBackgroundColor
+    if(valorBackgroundColor == "purple"){
+        princesa.style.borderRadius = "50%"
+        princesa.style.background = "radial-gradient(circle, rgba(144, 148, 244, 0.2) 0%, rgba(109, 113, 243, 0.5) 70%, rgba(144, 148, 244, 0) 100%)"
+    }else if (valorBackgroundColor == "blue"){
+        princesa.style.borderRadius = "50%"
+        princesa.style.background = "radial-gradient(circle, rgba(70, 130, 180, 0.2) 0%, rgba(70, 130, 180, 0.5) 70%, rgba(70, 130, 180, 0) 100%)"
+    }else if (valorBackgroundColor == "red"){
+        princesa.style.borderRadius = "50%"
+        princesa.style.background = "radial-gradient(circle, rgba(255, 0, 0, 0.2) 0%, rgba(255, 0, 0, 0.5) 70%, rgba(255, 0, 0, 0) 100%)"
     }else{
-        princesa.style.backgroundColor = ''
+        princesa.style.borderRadius = ''
+        princesa.style.background = ''
     }
     mudaPedra()
 })
@@ -92,30 +101,31 @@ valorLeftInput.addEventListener("keyup", ()=>{
     valorLeft = valorLeftInput.value.toLowerCase().trim()
 
     if(atravessou == 0){
-        if((valorLeft >= 140 && valorLeft <= 160) && (valorTop >= 740 && valorTop < 780) && valorZindex == ""){
+        if((valorLeft >= 140 && valorLeft <= 160) && (valorTop >= 630 && valorTop < 660) && valorZindex == ""){
+            parteRasa = 1
             princesa.style.left = `${valorLeft}px`
             setTimeout(() =>{
-                window.alert("Oh não, caso a princesa tente atravessar o rio ela irá se afogar, altere o z-index para ela levitar por cima do rio")
+                window.alert("Oh não, caso a princesa tente atravessar o rio ela irá se afogar, altere o z-index para ela levitar por cima do rio!!!")
             }, 1000)
-        }else if ((valorTop >= 740 && valorTop < 780) && valorZindex != ""){
+        }else if ((valorTop >= 630 && valorTop < 660) && valorZindex != ""){
             if(valorLeft >= 460){
                  princesa.style.left = `${valorLeft}px`
                  atravessou = 1
                  setTimeout(()=>{
-                    window.alert("Parabéns, você conseguiu atravessar a princesa. Agora é so chegar no castelo")
+                    window.alert("Parabéns, você conseguiu atravessar a princesa. Agora é so chegar no castelo!!!")
                  }, 1000)
                 princesa.style.left = `${valorLeft}px`
             }
-        }else if ((valorLeft >= 140 && valorLeft <= 160) && !(valorTop >= 760 && valorTop < 860) && valorZindex == ""){
-            window.alert("Você ainda não chegou na parte mais rasa do rio")
+        }else if ((valorLeft >= 140 && valorLeft <= 160) && !(valorTop >= 630 && valorTop < 660) && valorZindex == ""){
+            window.alert("Você ainda não chegou na parte mais rasa do rio!!!")
         }else if(valorLeft > 160 ){
-            window.alert("Valor muito alto para a travessia!")
+            window.alert("Valor muito alto para a travessia!!!")
         }else{
             princesa.style.left = `${valorLeft}px`
         }
     } else if (atravessou == 1){
         if(valorLeft > 500 ){
-            window.alert("Valor muito alto!")
+            window.alert("Valor muito alto!!!")
         }else{
             princesa.style.left = `${valorLeft}px`
         }
@@ -126,17 +136,19 @@ valorLeftInput.addEventListener("keyup", ()=>{
 valorZindexInput.addEventListener("keyup", ()=>{
     valorZindex = valorZindexInput.value.toLowerCase().trim()
 
-    if(valorZindex >= 2){
+    if(valorZindex >= 2 && parteRasa == 1){
         princesa.style.zIndex = valorZindex
         window.alert("A princesa está pronta para a travessia!!!")
     }else{
-        window.alert("A princesa irá se afogar!!!")
+        window.alert("A princesa irá se afogar, pois ainda não chegou na parte rasa!!!")
     }
 })
 
 function persistencia(){
-    const listaFasesCompletas = carregarFasesCompletas()
-    listaFasesCompletas.push("um")
+    const listaFasesCompletas = JSON.parse(localStorage.getItem("fasesConcluidas"))
+    if(!listaFasesCompletas.includes("sete")){
+        listaFasesCompletas.push("sete")
 
-    localStorage.setItem("fasesCompletas", JSON.stringify(listaFasesCompletas))
+        localStorage.setItem("fasesConcluidas", JSON.stringify(listaFasesCompletas))
+    }
 }
--- Mapas/MapaHistoria/main.js
+++ Mapas/MapaHistoria/main.js
@@ -1,5 +1,27 @@
 const fasesConcluidas = JSON.parse(localStorage.getItem("fasesConcluidas"))
+const modal = document.querySelector("#dialog-mensagem")
+const bntVoltar = document.querySelector("#btn-voltar")
+const btnExplorar = document.querySelector("#btn-explorar")
+const btnHistoria = document.querySelector("#btn-historia")
 
+bntVoltar.addEventListener("click", ()=>{
+    window.location.href = "../../index.html"
+})
+
+btnExplorar.addEventListener("click", ()=>{
+    window.location.href = "../../Mapas/MapaExplorar/mapaExplorar.html"
+})
+
+btnHistoria.addEventListener("click", ()=>{
+    localStorage.setItem("fasesConcluidas", JSON.stringify([]))
+    window.location.href = "../../Mapas/MapaHistoria/mapaHistoria.html"
+})
+
+if(fasesConcluidas.length > 6){
+    setTimeout(()=>{
+        modal.showModal()
+    }, 1000)
+}
 
 fasesConcluidas.forEach(idFase => {
     const divBotaoAlterar = document.querySelector(`#${idFase}`)
@@ -9,9 +31,5 @@ fasesConcluidas.forEach(idFase => {
     botao.textContent = "🏆"
     botao.classList.remove("learn-more")
     botao.classList.add("fase-concluida")
-    
-    
-        
 });
 
-// #F7EFBA
