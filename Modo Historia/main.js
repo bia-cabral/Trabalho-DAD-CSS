103b29ba2b48f7fc79881602ef0268a31c515ac9
Gustavo-Teotonio <gustavohidroar07@gmail.com>
2024-06-15 23:26:17 -0300

    Arrumando o stringfy

--- Modo Historia/main.js
+++ Modo Historia/main.js
@@ -1,1 +1,5 @@
-localStorage.setItem("fasesConcluidas", JSON.stringify([]))
+const fasesConcluidas = localStorage.getItem("fasesConcluidas")
+if(fasesConcluidas.length == 0 || fasesConcluidas == null){
+    localStorage.setItem("fasesConcluidas", JSON.stringify([]))
+}
+
